8adf2e185112c46f4dca00c89b9d6fd6671b5945
Jared Womack <womackjw@hotmail.com>
2019-06-26 15:58:25 -0700

    Added javascript dom manipulation and event listeners

--- ui-jay/src/index.js
+++ ui-jay/src/index.js
@@ -1,1 +1,14 @@
 import './index.scss'
+
+const alinks = document.querySelectorAll('a')
+
+alinks.forEach(link => {
+	link.addEventListener('mouseover', function(event) {
+		event.target.style.color = 'yellow'
+		event.target.style.border = '1px dashed black'
+	})
+})
+
+document.getElementById('jumbo-btn').style.color = '1px dashed yellow'
+
+console.log(alinks)
